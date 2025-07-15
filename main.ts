917d8799055dbb3bdd5d583d2fe3873da7ce055f
DeSullyMan3 <127885070+DeSullyMan3@users.noreply.github.com>
2025-07-15 19:27:25 +0200

    he can scream now

--- main.ts
+++ main.ts
@@ -1,9 +1,7 @@
 namespace SpriteKind {
     export const Bricks = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
+let y = 0
 let dir = 0
 let x = 80
 scene.setBackgroundImage(assets.image`bg`)
@@ -50,40 +48,56 @@ brick19.setPosition(148, 116)
 brick20.setPosition(156, 116)
 forever(function () {
     pyoro.setPosition(x, 105)
-    if (controller.left.isPressed()) {
-        x += -2
-        dir = 0
-    } else if (controller.right.isPressed()) {
-        x += 2
-        dir = 1
+    if (!(controller.A.isPressed())) {
+        if (controller.left.isPressed()) {
+            x += -2
+            dir = 0
+        } else if (controller.right.isPressed()) {
+            x += 2
+            dir = 1
+        }
     }
 })
 forever(function () {
-    if (dir == 0) {
-        if (controller.left.isPressed()) {
-            animation.runImageAnimation(
-            pyoro,
-            assets.animation`pyoro_walk_l`,
-            100,
-            true
-            )
-            pauseUntil(() => !(controller.left.isPressed()))
+    if (controller.A.isPressed()) {
+        y = 106
+        animation.stopAnimation(animation.AnimationTypes.All, pyoro)
+        if (dir == 0) {
+            pyoro.setImage(assets.image`pyoro_extend_l`)
         } else {
-            animation.stopAnimation(animation.AnimationTypes.All, pyoro)
-            pyoro.setImage(assets.image`pyoro_idle_l`)
+            pyoro.setImage(assets.image`pyoro_extend_r`)
         }
-    } else {
-        if (controller.right.isPressed()) {
-            animation.runImageAnimation(
-            pyoro,
-            assets.animation`pyoro_walk_r`,
-            100,
-            true
-            )
-            pauseUntil(() => !(controller.right.isPressed()))
+    }
+})
+forever(function () {
+    if (!(controller.A.isPressed())) {
+        y = 105
+        if (dir == 0) {
+            if (controller.left.isPressed()) {
+                animation.runImageAnimation(
+                pyoro,
+                assets.animation`pyoro_walk_l`,
+                100,
+                true
+                )
+                pauseUntil(() => !(controller.left.isPressed()))
+            } else {
+                animation.stopAnimation(animation.AnimationTypes.All, pyoro)
+                pyoro.setImage(assets.image`pyoro_idle_l`)
+            }
         } else {
-            animation.stopAnimation(animation.AnimationTypes.All, pyoro)
-            pyoro.setImage(assets.image`pyoro_idle_r`)
+            if (controller.right.isPressed()) {
+                animation.runImageAnimation(
+                pyoro,
+                assets.animation`pyoro_walk_r`,
+                100,
+                true
+                )
+                pauseUntil(() => !(controller.right.isPressed()))
+            } else {
+                animation.stopAnimation(animation.AnimationTypes.All, pyoro)
+                pyoro.setImage(assets.image`pyoro_idle_r`)
+            }
         }
     }
 })
